6790202a80dfe9b5fe952f8387a43a9602f6beb5
Maurice Debusson <maurice.debusson@live.de>
2024-07-28 17:58:42 +0200

    add command.txt to git an change command URL

--- runcommand.sh
+++ runcommand.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-CMD_URL="http://defaultscripts.rf.gd/cmd.txt" ## You have to change the URL or i can control your Server ;-)
+CMD_URL="https://raw.githubusercontent.com/ChuckMorrisGit/defaultScripts/main/command.txt" ## You have to change the URL or i can control your Server ;-)
 
 CMD=$(exec curl -s $CMD_URL &)  
 
