d998e54a8d0281b48ac953c816112917e817a9ae
Maurice Debusson <maurice.debusson@live.de>
2025-01-11 19:04:17 +0100

    Refactor heartbeat.sh to update from Git before executing heartbeat.py

--- heartbeat.sh
+++ heartbeat.sh
@@ -8,13 +8,16 @@ EXITCODE=1
 #while [ $EXITCODE -ne 0 ]
 while [ true ]
 do
+    bash ./updateFromGit.sh
+
+    
     ./heartbeat.py
     EXITCODE=$?
 
     echo "Heartbeat exited with code $EXITCODE. Restarting in 3 seconds..."
     sleep 3
 
-    bash ./updateFromGit.sh
+    
 
 done
 
